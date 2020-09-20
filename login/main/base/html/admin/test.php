f2c5c24fc3f33daf742c0407bd1d7dedfcf68cda
Loximity-oss <67264198+Loximity-oss@users.noreply.github.com>
2020-09-21 03:16:29 +0800

    Finished announcement system.

--- login/main/base/html/admin/test.php
+++ login/main/base/html/admin/test.php
@@ -4,7 +4,9 @@
       echo  mysqli_connect_error();
       exit;
   }
+  
   print_r($_POST);
   print_r($_FILES);
-
+  $sql = "UPDATE `announcement` SET `Title` = '" . $_POST['ancTitleID'] . "', `Text` = '" . $_POST['ancContent'] . "' WHERE `announcement`.`AnnouncmentID` = '" . $_POST['staticancID'] . "'";
+  echo $sql;
 ?>
